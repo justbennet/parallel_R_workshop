aa3040c9cd9cb4148bf81b0ebb3f50af75d8f9ea
Bennet Fauber <bennet@umich.edu>
2014-07-26 17:28:54 -0400

    Fix typo in group.* names

--- t_power/02power_function.R
+++ t_power/02power_function.R
@@ -16,7 +16,7 @@ t.power.sim = function(n, n.reps=1000, mean.1=0.0, mean.2=2.5, sd.common=1.0) {
   for (i in 1:n.reps){
     group.1 = rnorm(n=n, mean=mean.1, sd=sd.common)
     group.2 = rnorm(n=n, mean=mean.2, sd=sd.common)
-    p.vals[i] = t.test(group.x1, group.x2)$p.value
+    p.vals[i] = t.test(group.1, group.2)$p.value
   }
   power = sum(p.vals < .05)/n.reps
   return(power)
